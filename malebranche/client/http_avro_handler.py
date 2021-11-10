2ee0cc013f8681a40c1dc26382a9ecfb064dafae
Alberto Amigo Alonso <albamigh@gmail.com>
2021-11-10 12:20:23 +0100

    - Schema set
    issue #6

--- malebranche/client/http_avro_handler.py
+++ malebranche/client/http_avro_handler.py
@@ -1,5 +1,38 @@
+from io import BytesIO
 from logging import LogRecord
 from logging.handlers import HTTPHandler
+
+from fastavro import writer
+
+schema = {
+    'name': 'log_schema',
+    'type': 'record',
+    'fields': [
+        {'name': 'message', 'type': 'string'},
+        {'name': 'name', 'type': 'string'},
+        {'name': 'msg', 'type': 'string'},
+        {'name': 'name', 'type': 'string'},
+        {'name': 'args', 'type': 'string'},
+        {'name': 'levelname', 'type': 'string'},
+        {'name': 'levelno', 'type': 'string'},
+        {'name': 'pathname', 'type': 'string'},
+        {'name': 'filename', 'type': 'string'},
+        {'name': 'module', 'type': 'string'},
+        {'name': 'exc_info', 'type': 'string'},
+        {'name': 'exc_text', 'type': 'string'},
+        {'name': 'stack_info', 'type': 'string'},
+        {'name': 'lineno', 'type': 'string'},
+        {'name': 'funcName', 'type': 'string'},
+        {'name': 'created', 'type': 'string'},
+        {'name': 'msecs', 'type': 'string'},
+        {'name': 'relativeCreated', 'type': 'string'},
+        {'name': 'thread', 'type': 'string'},
+        {'name': 'threadName', 'type': 'string'},
+        {'name': 'processName', 'type': 'string'},
+        {'name': 'process', 'type': 'string'},
+        {'name': 'parent', 'type': 'string'},
+    ],
+}
 
 
 class HttpAvroHandler(HTTPHandler):
@@ -8,13 +41,14 @@ class HttpAvroHandler(HTTPHandler):
             import urllib.parse
 
             host = self.host
-            h = self.getConnection(host)
+            h = self.getConnection(host, self.secure)
             url = self.url
-            data = urllib.parse.urlencode(self.stack)
+            data = BytesIO()
+            writer(data, schema, self.mapLogRecord(record))
             h.putrequest(self.method, url)
 
             if self.method == "POST":
-                h.putheader("Content-type", "application/x-www-form-urlencoded")
+                h.putheader("Content-type", "avro/binary")
                 h.putheader("Content-length", str(len(data)))
 
             h.endheaders()
