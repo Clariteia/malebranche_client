e1978fd3bc14e50bb0e85c77d7fa8c7552ee31e9
Alberto Amigo Alonso <albamigh@gmail.com>
2021-11-17 14:40:35 +0100

    - Added basic test.
    issue #13

--- malebranche/client/logger/logger.py
+++ malebranche/client/logger/logger.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import logging
+from logging import Handler
 
 from malebranche.client.context import ContextManager
 from .filter import MalebrancheLogFilter
@@ -8,17 +9,29 @@ from .http_avro_handler import HttpAvroHandler
 
 
 class Logger:
-    def __init__(self, module: str, context: ContextManager, level=logging.DEBUG):
-        self.logger: logging.Logger = self._create_logger(module, context, level)
+    def __init__(
+        self,
+        module: str,
+        context: ContextManager,
+        level=logging.DEBUG,
+        handler: Handler = HttpAvroHandler(host="localhost:5000", url="/logs", method="POST")
+    ):
+        self.logger: logging.Logger = self._create_logger(module, context, level, handler)
 
-    def _create_logger(self, module: str, context, level=logging.DEBUG) -> logging.Logger:
+    def _create_logger(
+        self,
+        module: str,
+        context,
+        level=logging.DEBUG,
+        handler: Handler = HttpAvroHandler(host="localhost:5000", url="/logs", method="POST")
+    ) -> logging.Logger:
         logging.basicConfig()
         logger = logging.getLogger(module)
         if logger.hasHandlers():
             logger.handlers.clear()
             logger.filters.clear()
 
-        network_handler = HttpAvroHandler(host="localhost:5000", url="/logs", method="POST")
+        network_handler = handler
         logger.addHandler(network_handler)
         old_level = logger.getEffectiveLevel()
         logger.addFilter(MalebrancheLogFilter(context=context))
--- tests/test_logger/test_logger.py
+++ tests/test_logger/test_logger.py
@@ -1,13 +1,18 @@
 import unittest
+from logging import INFO
 
+from malebranche.client.context import ContextManager
 from malebranche.client.logger.logger import Logger
 
 
-class MyTestCase(unittest.TestCase):
+class TestLogger(unittest.TestCase):
     def setUp(self) -> None:
-        logger = Logger
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+        context = ContextManager()
+        self.logger = Logger(__name__, context)
+
+    def test_info(self):
+        with self.assertLogs(__name__, level=INFO):
+            self.logger.info("test message")
 
 
 if __name__ == '__main__':
