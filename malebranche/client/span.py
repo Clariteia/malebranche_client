5d043985e89d518ec0b3ed92fae0fcbf985295e2
Alberto Amigo Alonso <albamigh@gmail.com>
2021-11-18 16:26:55 +0100

    - Solved further issues.
    issue #11

--- malebranche/client/span.py
+++ malebranche/client/span.py
@@ -8,24 +8,26 @@ SPANS_TREE = ContextVar("malebranche.spans")
 
 
 class Span:
-    def __init__(self, logger):
-        self.logger = logger
+    def __init__(self):
+        self.logger = None
         self.is_root = False
         self.token = None
         self.context = None
 
     def __enter__(self):
         try:
-            context: ContextManager = SPANS_TREE.get()
+            self.context: ContextManager = SPANS_TREE.get()
         except LookupError:
             self.is_root = True
             self.context: ContextManager = ContextManager()
             self.token = SPANS_TREE.set(self.context)
         else:
-            context.add_child_process()
-            SPANS_TREE.set(context)
+            self.context.add_child_process()
+            SPANS_TREE.set(self.context)
 
         self.logger = Logger(__name__, self.context, "INFO")
+
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         if self.is_root:
